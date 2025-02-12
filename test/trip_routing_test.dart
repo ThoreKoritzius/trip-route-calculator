d34ffa80f4b31293b7e68865b94dea5823d9d6bb
Thore Koritzius <thorekoritzius@outlook.de>
2025-02-12 07:24:24 +0100

    add testing

--- test/trip_routing_test.dart
+++ test/trip_routing_test.dart
@@ -1,12 +1,58 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:trip_routing/trip_routing.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('TripService', () {
+    final waypoints = [
+      const LatLng(50.77437074991441, 6.075419266272186),
+      const LatLng(50.774717242584515, 6.083980842518867),
+    ];
+
+    final routing = TripService();
+
+    setUpAll(() async {
+      // Make routing available offline
+      await routing.useCity('Aachen');
+    });
+
+    test('findTotalTrip returns a route with distance', () async {
+      final trip = await routing.findTotalTrip(
+        waypoints,
+        preferWalkingPaths: true,
+        replaceWaypointsWithBuildingEntrances: true,
+      );
+
+      // Ensure the route is not empty
+      expect(trip.route.isNotEmpty, true);
+
+      // Check if distance is greater than zero
+      expect(trip.distance, greaterThan(0));
+
+      // There should be no errors
+      expect(trip.errors, isEmpty);
+    });
+
+    test('findTotalTrip handles errors gracefully', () async {
+      //unreachable waypoints
+      final invalidWaypoints = [
+        const LatLng(0, 0),
+        const LatLng(0, 0),
+      ];
+
+      try {
+        final trip = await routing.findTotalTrip(
+          invalidWaypoints,
+          preferWalkingPaths: true,
+          replaceWaypointsWithBuildingEntrances: true,
+        );
+
+        // If no exception is thrown, check for errors in the result
+        expect(trip.errors.isNotEmpty, true);
+      } catch (e) {
+        // If an exception is thrown, it's also acceptable
+        expect(e, isNotNull);
+      }
+    });
   });
 }
